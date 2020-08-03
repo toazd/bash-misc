48c776ec0b8656932891cd7ba3742484a95a1225
William <wmcdannell@hotmail.com>
2020-08-02 20:46:50 -0400

    fix to support dotfiles, various minor fixes and optimizations, cleanup debug stuff, update comments

--- copy-files-to-folders-based-on-ext.sh
+++ copy-files-to-folders-based-on-ext.sh
@@ -4,49 +4,60 @@
 # NOTE the path files are copied/moved to and files in it are ignored during searching (in case the sMOVE_TO_PATH is within sSEARCH_PATH)
 # NOTE the script that is run is ignored during file searching TODO more testing to ensure the check works in all cases
 # NOTE duplicate file checks are based first on case-sensitive file names, followed by checksums IF the names match
-#      this means that if you have files with the same contents, but with different names
-#      they will be copied
+#      this means that if you have files with the same contents, but with different names they will be copied
+#
 # BUG  When using * as a search pattern (the default) files named like "APACHE-LICENSE-2.0" and "spotify-1.1.10.546-Release"
 #      will be put into a folder named based on the characters following the right-most dot "." found in the name
 #      eg. "0" and ".546-Release"
 #
-# TODO parameter handling and usage output
-#
 
 shopt -s globstar nullglob
 
+# TODO parameter handling and usage output
 # All parameters are optional
 # First parameter = /search/path (default: the current working directory) NOTE this does not have to be the script path
-# Second parameter = extension (default: *) NOTE the pattern searched becomes *.extension
+# Second parameter = extension (default: *)
 # Third parameter = /path/to/copyto (default: "sandbox/move_ext_test") NOTE this is relative to the current working directory not the script path
 
 sSEARCH_PATH=${1:-"."}
 sFILE_EXT=${2:-"*"}
 sMOVE_TO_PATH=${3:-"sandbox/move_ext_test"}
+sMOVE_TO_PATH=${sMOVE_TO_PATH//.\/}
 iFILE_COUNTER=0
 
 # TODO remove after testing is "done"
 #rm -r "$sMOVE_TO_PATH"
 
-# NOTE You could use *.$sFILE_EXT (NOT *."$sFILE_EXT") which would likely
-# be faster, but then you run the risk that if any IFS characters appear
-# in the given extension the script does not behave as expected
-# alternatively, the faster method could be used if a sanity check is done on
-# sFILE_EXT before using it for processing
-for sFILENAME in "$sSEARCH_PATH"/**/*.*; do
+# check for write access to the move/copy path
+# NOTE if sMOVE_TO_PATH is NULL here, then "./" was specified
+if [[ $sMOVE_TO_PATH = "" ]]; then
+    sMOVE_TO_PATH=$PWD
+    [[ ! -w $sMOVE_TO_PATH ]] && { echo "No write access to ${sMOVE_TO_PATH%%/*}"; exit 1; }
+else
+    [[ ! -w ${sMOVE_TO_PATH%%/*} ]] && { echo "No write access to ${sMOVE_TO_PATH%%/*}"; exit 1; }
+fi
+
+# TODO find out exactly why * does not match dotfiles by itself
+for sFILENAME in "$sSEARCH_PATH"/**/{*,.*}; do
+
+    # If no files are found, having nullglob turned on causes the pattern to expand to NULL instead of itself
+    [[ -z $sFILENAME ]] && continue
 
     # Replace all instances of dot forward slash "./" with NULL
-    # in both the filename found and the specified sMOVE_TO_PATH
     sFILENAME=${sFILENAME//.\/}
-    sMOVE_TO_PATH=${sMOVE_TO_PATH//.\/}
 
-    # If sFILENAME is not a folder and
-    # If we have write access to the file (for move, only read is needed for copy) and
-    # the file name (in all lower-case) matches the pattern *.extension (all lower-case) and
-    # NOTE if *.* is changed to *.$sFILE_EXT, remove the extension pattern check here
-    # If the move path is not part of the file name and
-    # If the filename is not equal to the script name
-    if [[ ! -d $sFILENAME && -w $sFILENAME && ${sFILENAME,,} == *.${sFILE_EXT,,} && ! $sFILENAME == *$sMOVE_TO_PATH* && ! $sFILENAME == *${0//.\/} ]]; then
+    # Filter through the results
+    # If sFILENAME is not a folder (eg. "." & "..") and
+    # If we have read access to the file and TODO change to -w for move
+    # the file path and name (in all lower-case) matches the pattern *.extension (all lower-case) and
+    # If the move path is not part of the file name (exclude move path and all files in it) and
+    # If the filename is not equal to the script name (with dot forward-slash "./" removed)
+    if [[ ! -d $sFILENAME && \
+          -r $sFILENAME && \
+          ${sFILENAME,,} == *.${sFILE_EXT,,} && \
+          ! $sFILENAME == *$sMOVE_TO_PATH* && \
+          ! $sFILENAME == *${0//.\/} \
+          ]]; then
 
         # Get the basename of the file not including the extension
         sBASENAME_NO_EXT=${sFILENAME%.*}
@@ -68,6 +79,7 @@ for sFILENAME in "$sSEARCH_PATH"/**/*.*; do
         # but differ in case will be put inside the same folder irregardless of extension case
         # eg. "file.txt", "file.TxT", "file.TXt", "file.TXT", and "file.tXT" will all be put
         # into the same folder named "txt"
+        # If you don't want that behavior, change sLOWERCASE_EXT to sEXT where appropriate below
         mkdir -p "$sMOVE_TO_PATH/$sLOWERCASE_EXT"
 
         # Check for a possible file name conflict
@@ -79,12 +91,20 @@ for sFILENAME in "$sSEARCH_PATH"/**/*.*; do
             [[ -d "${sMOVE_TO_PATH}/${sLOWERCASE_EXT}" && -w "${sMOVE_TO_PATH}/${sLOWERCASE_EXT}" ]] && \
                 cp "$sFILENAME" "${sMOVE_TO_PATH}/${sLOWERCASE_EXT}" && iFILE_COUNTER=$((iFILE_COUNTER+1))
         else
+
+            # The name of a file in the same move path we want to copy/move to matches the current file
             echo "Duplicate file name detected: $sFILENAME"
+
+            # If they are hardlinks they are already the same, no need to compare checksums
             [[  $sFILENAME -ef "${sMOVE_TO_PATH}/${sLOWERCASE_EXT}/${sBASENAME}" ]] && {
                 echo "Hardlink detected, skipping copy"
                 continue
             }
 
+            # Compare checksums and see if there is an exact copy (content but not name wise) already
+            # NOTE to narrow down the number of files to compare against, not all files are checked
+            # TODO determine an appropriate scope for the comparison
+            # TODO different checksum algorithm?
             sDUPLICATE=$(sha512sum "$sFILENAME")
             for sFILE in "${sMOVE_TO_PATH}/${sLOWERCASE_EXT}/${sBASENAME_NO_EXT}"*; do
                 sEXISTING=$(sha512sum "$sFILE")
