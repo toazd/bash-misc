872e18aa0046c5a69fbfca6dbab19578326ecf4a
William <wmcdannell@hotmail.com>
2020-08-03 14:02:09 -0400

    remove external command requirements, cleanup

--- count-files-folders-symlinks-recursively.sh
+++ count-files-folders-symlinks-recursively.sh
@@ -2,35 +2,34 @@
 #
 # Toazd 2020 Unlicense
 #
-# Find and count files, folders, and symlinks using mostly bash
+# Find and count files, folders, and symlinks using only bash built-ins
 #
 
 shopt -s nullglob extglob globstar dotglob
 set -eEBu
 
-sSEARCH_PATH=${1:-"$(pwd -P)"}
+sSEARCH_PATH=${1:-"$(builtin pwd -P)"}
 iaFILES=()
 iaDIRS=()
 iaLINKS=()
 sNODE=""
-# If you want to count the search_path in the results like find does set iMIMIC_FIND to 1
-iMIMIC_FIND=0
+iMIMIC_FIND=0 # If you want to count the search_path in the results like find does set iMIMIC_FIND to 1
 
-# If a file is specified as the search path, use it's parent path
-[[ -f $sSEARCH_PATH ]] && sSEARCH_PATH="$(dirname "$(realpath "$sSEARCH_PATH")")"
+# If a file is specified as the search path
+[[ -f $sSEARCH_PATH ]] && { echo "$sSEARCH_PATH is a file, please specify a path"; exit 1; }
 
 # If search path is not a folder that we can read from, abort
-[[ -d $sSEARCH_PATH || -r $sSEARCH_PATH ]] || { echo "$sSEARCH_PATH is a not directory or cannot be read"; exit; }
+[[ -d $sSEARCH_PATH || -r $sSEARCH_PATH ]] || { echo "$sSEARCH_PATH is not a valid path or cannot be read"; exit 1; }
 
-# Remove trailing / if present
-[[ ${sSEARCH_PATH:${#sSEARCH_PATH}-1:1} = "/" ]] && sSEARCH_PATH="${sSEARCH_PATH:0:${#sSEARCH_PATH}-1}"
+# Remove a trailing forward-slash "/" if it is present
+[[ ${sSEARCH_PATH:${#sSEARCH_PATH}-1:1} = "/" ]] && sSEARCH_PATH=${sSEARCH_PATH:0:${#sSEARCH_PATH}-1}
 
 # NOTE find -type d will return the search_path in the results
 # to mimic that behavior, set iMIMIC_FIND=1
 (( iMIMIC_FIND )) && iaDIRS+=("$sSEARCH_PATH")
 
 for sNODE in "$sSEARCH_PATH"/**/*; do
-    [[ $(basename "$sNODE") = @(""|"."|"..") ]] && continue
+    [[ ${sNODE##*/} = @(""|"."|"..") ]] && continue
     [[ -h $sNODE ]] && { iaLINKS+=("${sNODE}"); continue; }
     [[ -f $sNODE ]] && { iaFILES+=("${sNODE}"); continue; }
     [[ -d $sNODE ]] && { iaDIRS+=("${sNODE}"); continue; }
