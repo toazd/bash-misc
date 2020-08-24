f64ff9387b4219bfac9bae4f4524328b80202b21
William <wmcdannell@hotmail.com>
2020-08-24 12:23:50 -0400

    cleanup

--- sandbox/vkBasalt-shaders.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sSHADERS_PATH="/usr/share/reshade/shaders"
-for sFILE in "$sSHADERS_PATH"/*; do
-    [[ ${sFILE,,} == *.fx ]] && {
-        printf "%s = %s\n" "$(basename "${sFILE,,}" .fx)" "\"$sFILE\""
-    }
-done
